c197d6e22bd621b822e343b71e2dba1017518edf
amirmehdi jafari fesharaki <amiroo23jf@gmail.com>
2022-08-09 23:04:28 +0430

    Remove extra files

--- UE.py
+++ UE.py
@@ -9,7 +9,9 @@ class UE():
         self.uid =  user_info["uid"]
         self.interval = user_info["interval"]
         self.locations= user_info["locations"]
-
+        self.data_socket = None
+        self.close_connection_lock = threading.Lock()
+        self.close_connection = False
         # simulation timing configurations
         self.sim_started = False
         self.start_time = None 
@@ -32,7 +34,7 @@ class UE():
         
         # sending signaling channel setup message
         msg = {"type":3, "message": self.uid}
-        msg_json = json.dumps(msg, indent=4)
+        msg_json = json.dumps(msg, indent=4) + "\END_OF_MSG"
         logging.debug("UE(" + str(self.uid) + "): Sending the Signaling Channel Setup message to port:("+str(enb_addr[1])+")")
         s.sendall(msg_json.encode("utf-8"))
 
@@ -43,12 +45,93 @@ class UE():
 
         #logging.debug("UE("+str(self.uid)+"): Simulation is started")
         # Second Stage: Simulation is started the position should be sent 
+
+        # creating the list of location update times
+        locations = self.locations.copy()
+        interval = float(self.interval[:-1])
+        update_times = list()
+        t0 = 0
+        for i in range(len(locations)):
+            update_times.append(t0)
+            t0 = t0 + interval
+
+        socket_lock = threading.Lock()
+        signaling_handler = threading.Thread(target=self.handle_signaling, args=(s, socket_lock))
+        signaling_handler.start()
         while True:
-            pass
+            if (len(update_times)>=1):
+                next_update_time = update_times[0]
+                new_location = locations[0]
+                current_time = time.time() - self.start_time
+                if (current_time >= next_update_time):
+                    socket_lock.acquire()
+                    self.send_new_location(new_location, next_update_time, enb_addr[1], s)
+                    socket_lock.release()
+                    locations.pop(0)
+                    update_times.pop(0)
+            
+    def handle_signaling(self, s, socket_lock):
+        '''Handles the signals recieved from the eNodeB'''
+        while True:
+            # data recieved from client
+            data = s.recv(2048)
+            if not data:
+                # the connection was closed
+                break
+
+            # decoding the data
+            data_recieved = data.decode('utf-8').split("\END_OF_MSG")
+            for data_decoded in data_recieved[:-1]:
+                try:
+                    if (len(data_decoded)!=0):
+                        data_dict = json.loads(data_decoded)
+                except:
+                    logging.warning("UE("+str(self.uid)+"): Data recieved from ENB is corrupted")
+                if (data_dict["type"] == 7):
+                    # create a new data channel
+                    enb_port = data_dict["message"]["port"]
+                    logging.debug("UE("+str(self.uid)+"): New data channel should be created with ENB on port:(" + str(enb_port) + ")")
+                    if self.data_socket == None:
+                        self.create_data_channel(enb_port)
+                    else: 
+                        # close connection on socket self.data_socket and create the new data channel
+                        self.close_connection_lock.acquire()
+                        self.close_connection = True
+                        self.close_connection_lock.release()
+                        while (self.close_connection):
+                            pass
+                        self.create_data_channel(enb_port)
+                
+
+    def create_data_channel(self,enb_port):
+        '''Creates a new data channel with a thread for receiving data and a thread for sending data'''
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection_status = s.connect_ex(('127.0.0.1',enb_port))
+        while (connection_status != 0):
+            connection_status = s.connect_ex('127.0.0.1',enb_port)
+        logging.info("UE("+str(self.uid)+"): Data Channel with ENB on port:("+str(enb_port)+") is established")
+        self.data_socket = s
+        while True:
+            self.close_connection_lock.acquire()
+            if (self.close_connection):
+                s.close()
+                logging.info("UE("+str(self.uid)+"): Data Channel with ENB on port:("+str(enb_port)+") is closed")
+                self.close_connection = False
+                self.close_connection_lock.release()
+                break
+            self.close_connection_lock.release()
+
 
     def start_simulation(self, start_time):
         '''This method is called by LTESimulator when the simulation is started by giving the start time of 
         the simulation and setting the sim_started parameter to True'''
         self.start_time = start_time
         self.sim_started = True
-        
+
+    def send_new_location(self, new_location, current_time, port, s):
+        '''Sending the new location to the given port using the socket "s"''' 
+        # sending location announcement message
+        msg = {"type":0, "message":{"position":new_location, "uid":self.uid, "time":current_time}}
+        msg_json = json.dumps(msg, indent=4) + "\END_OF_MSG"
+        logging.debug("UE("+str(self.uid)+"): Sending position announcement in time:("+"{:.2f}".format(current_time)+") to port:("+str(port)+")")
+        s.sendall(msg_json.encode("utf-8"))
--- eNodeB.py
+++ eNodeB.py
@@ -11,9 +11,13 @@ class eNodeB():
         self.uid = uid
         self.host = "127.0.0.1"
 
-        # initializing the dictionaries
-        self.lock_dict = {"ue_id_port_dict" : threading.Lock(),}
-        self.ue_id_port_dict = dict()
+        # initializing the database
+        self.database = dict()
+
+        # initializing the locks
+        self.db_lock = threading.Lock()
+        self.mme_send_lock = threading.Lock()
+        self.covered_users = []
 
         # simulation timing configurations
         self.sim_started = False
@@ -30,8 +34,9 @@ class eNodeB():
 
         logging.info("ENB("+str(self.uid)+"): Connection with SGW is established on port:(%d)", sgw_port)
         # sending a eNodeB-SGW Connection message
+        self.sgw_socket = s
         msg = {"type":1, "message": self.uid}
-        msg_json = json.dumps(msg, indent=4)
+        msg_json = json.dumps(msg) + "\END_OF_MSG"
         logging.debug("ENB("+str(self.uid)+"): Sending the eNodeB-SGW connection message")
         s.sendall(msg_json.encode("utf-8"))
         while True :
@@ -45,13 +50,55 @@ class eNodeB():
             connection_status = s.connect_ex((HOST, mme_port))
 
         logging.info("ENB("+str(self.uid)+"): Connection with MME is established on port:(%d)", mme_port)
+        self.mme_socket = s
         # sending a eNodeB-MME Connection message
         msg = {"type":2, "message": self.uid}
-        msg_json = json.dumps(msg, indent=4)
+        msg_json = json.dumps(msg) + "\END_OF_MSG"
         logging.debug("ENB("+str(self.uid)+"): Sending the eNodeB-MME connection message")
         s.sendall(msg_json.encode("utf-8"))
         while True :
-            pass
+            # data recieved from client
+            data = s.recv(2048)
+            if not data:
+                # the connection was closed
+                break
+
+            # decoding the data
+            data_decoded = data.decode("utf-8")
+            data_recieved = data.decode('utf-8').split("\END_OF_MSG")
+            for data_decoded in data_recieved[:-1]:
+                try:
+                    if (len(data_decoded)!=0):
+                        data_dict = json.loads(data_decoded)
+                except:
+                    logging.warning("ENB("+str(self.uid)+"): Data recieved from MME is corrupted")
+                if (data_dict["type"] == 6):
+                    user_uid = data_dict["message"]["uid"]
+                    has_previous = data_dict["message"]["has_previous"]
+                    if (has_previous):
+                        previous_enb = data_dict["message"]["previous_enb"]
+                        logging.info("ENB("+str(self.uid)+"): UE("+str(user_uid)+") is in my cell and it was previously connected to ENB("+
+                        str(previous_enb)+")")
+                    else:
+                        logging.info("ENB("+str(self.uid)+"): UE("+str(user_uid)+") is in my cell and it had no previous ENB")
+                    self.covered_users.append(user_uid)
+                    # send User-eNodeB Connection
+                    msg = {"type":7, "message": {"port":self.data_port}}
+                    self.send_signal_to_user(user_uid, msg)
+                    
+
+                elif (data_dict["type"] == 8):
+                    # Release connection
+                    user_uid = data_dict["message"]["uid"]
+                    next_enb = data_dict["message"]["next_enb"]
+                    self.covered_users.remove(user_uid)
+                    logging.info("ENB("+str(self.uid)+"): Data channel with UE("+str(user_uid)+") is closed and it is now connected to ENB("
+                    +str(next_enb)+")")
+                    
+                    ##### Fill this part to buffer the data received ####
+
+
+                    
 
     def run_server(self, port, signaling, max_clients=100):
         if signaling:  
@@ -96,18 +143,43 @@ class eNodeB():
                 break
             
             # decoding the data
-            data_decoded = data.decode("utf-8")
-            data_dict = json.loads(data_decoded)
-            if (data_dict["type"] == 3):
-                client_uid = data_dict["message"]
-                logging.info("ENB("+str(self.uid)+"): UE(" + str(client_uid) + ") is connected from port:(" + str(client_port) + ")")
-                self.lock_dict["ue_id_port_dict"].acquire()
-                self.ue_id_port_dict[client_uid] = client_port
-                self.lock_dict["ue_id_port_dict"].release()
+            data_recieved = data.decode('utf-8').split("\END_OF_MSG")
+            for data_decoded in data_recieved[:-1]:
+                try:
+                    if (len(data_decoded)!=0):
+                        data_dict = json.loads(data_decoded)
+                    else :
+                        data_dict = None
+                except:
+                    logging.warning("ENB("+str(self.uid)+"): Data recieved from UE("+str(client_uid)+") is corrupted")
+                if data_dict is None:
+                    pass
+                elif (data_dict["type"] == 3):
+                    client_uid = data_dict["message"]
+                    logging.info("ENB("+str(self.uid)+"): UE(" + str(client_uid) + ") is connected from port:(" + str(client_port) + ")")
+                    self.db_lock.acquire()
+                    self.database[client_uid] = {"signaling_socket": c, "signaling_lock": threading.Lock()}
+                    self.db_lock.release()
+                
+                elif (data_dict["type"] == 0):
+                    # saving the distance between the UE and ENB in the database
+                    new_position = data_dict["message"]["position"]
+                    distance = self.calculate_distance(new_position)
+                    user_time = data_dict["message"]["time"]
+                    self.db_lock.acquire()
+                    self.database[client_uid]["distance"] = distance
+                    self.db_lock.release()
+
+                    # sending a user distance message to the MME
+                    msg = {"type":5, "message": {"distance":distance,"user_uid":client_uid, "user_time":user_time}}
+                    self.send_to_mme(msg)
+                    logging.debug("ENB("+str(self.uid)+"): Sending User Distance of UE(" + str(client_uid)+") to MME")
+
+
         return 
     
     def handle_data_nodes(self, c, addr):
-        client_uid = 0
+        client_uid = None
         client_port = addr[1]
         while True:
 
@@ -116,10 +188,49 @@ class eNodeB():
             if not data:
                 # the connection was closed
                 break
-        return 
+            
+            # decoding the data
+            data_recieved = data.decode('utf-8').split("\END_OF_MSG")
+            for data_decoded in data_recieved[:-1]:
+                try:
+                    if (len(data_decoded)!=0):
+                        data_dict = json.loads(data_decoded)
+                    else :
+                        data_dict = None
+                except:
+                    logging.warning("ENB("+str(self.uid)+"): Data recieved from UE("+str(client_uid)+") is corrupted")
+                if data_dict is None:
+                    pass
+        
 
     def start_simulation(self, start_time):
         '''This method is called by LTESimulator when the simulation is started by giving the start time of 
         the simulation and setting the sim_started parameter to True'''
         self.start_time = start_time
         self.sim_started = True
+
+    def calculate_distance(self, new_position):
+        '''Calculates the distance between the new position and the position of the eNodeB'''
+        x_pos = new_position[0]
+        y_pos = new_position[1]
+
+        distance = ((x_pos - self.location[0])**2 + (y_pos - self.location[1])**2)**0.5
+        return distance
+
+    def send_to_mme(self, msg):
+        '''Sends a message to the MME server'''
+        msg_json = json.dumps(msg) + "\END_OF_MSG"
+        self.mme_socket.sendall(msg_json.encode("utf-8"))
+
+    def send_signal_to_user(self, user_uid, msg):
+        '''Sends a message to the user  with the given user_uid and msg'''
+        msg_json = json.dumps(msg) + "\END_OF_MSG"
+        c, socket_lock = self.database[user_uid]["signaling_socket"], self.database[user_uid]["signaling_lock"]
+        socket_lock.acquire()
+        c.sendall(msg_json.encode("utf-8"))
+        socket_lock.release()
+
+        
+
+
+    
